01f801cbf139078300ff0ddf24c629c698988bb3
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2024-09-21 16:18:10 -0600

    Data and Servoces Page - Data for services page create - Data for education and certifications created - Rough draft of Services page created

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,29 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+import { servicesPackages } from "../content/Services-content";
+
 export default function Services() {
    return (
     <div className="app">
-      Services
+      <Header />
+      {servicesPackages.map((service, index) => {
+        let title = service.title;
+        return (
+          <div key={`${title}-service`}>
+            <h3>{title}</h3>
+            <p>{service.description}</p>
+            <h4>{service.price}</h4>
+            <ul>
+              {service.list.map((bullet, index) => {
+                return (
+                  <li key={`${title}-service-bullet-${index}`}>{bullet}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
+      <Footer />
     </div>
    )
 }
