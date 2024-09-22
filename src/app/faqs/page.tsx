da641d1739dee2357385d4c969185f62bd477110
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2024-09-21 18:51:01 -0600

    Fixed Homepage Service Links - Service links on homepage now go directly to targeted services - Had to add "id" element to servicePackages objects - Assigned specific ID to the h3 title in the /services/page.tsx using new element in the servicesPackage data structure

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,11 +1,12 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
+import { birthfaqs, postpartumfaqs, lactationfaqs } from "../content/Faq-content";
 
 export default function faqs() {
   return (
     <div className="app">
       <Header />
-      FAQ's Page
+
       <Footer />
     </div>
   );
